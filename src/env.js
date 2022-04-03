3aaaac1d08639d82b87a1856d79fd7dbcc9c5a9f
Keerthi Shankar Subramanian <keerthi.subramanian@mitigram.com>
2022-04-03 20:58:12 +0200

    Add module for reading env variables

---
+++ src/env.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+
+const env = function environmentVariables() {
+    const SHOULD_PUBLISH_JOB_STATUS = 'SHOULD_PUBLISH_JOB_STATUS';
+    let allEnvs = {
+        SHOULD_PUBLISH_JOB_STATUS
+    };
+    allEnvs.SHOULD_PUBLISH_JOB_STATUS = process.env.SHOULD_PUBLISH_JOB_STATUS;
+
+    // Debug logging
+    core.debug('Environment variables:');
+    Object.keys(allEnvs).forEach(item => {
+        core.debug(`${item}: ${allEnvs[item]}`);
+    });
+    core.debug('');
+
+    return {
+        SHOULD_PUBLISH_JOB_STATUS
+    };
+};
+
+module.exports = env;
