f1a46cd4433483ddaa436ce981a163636f98ab09
Keerthi Shankar Subramanian <keerthi.subramanian@mitigram.com>
2022-04-03 12:52:26 +0200

    Add logging

--- src/requests/post-request.js
+++ src/requests/post-request.js
@@ -1,12 +1,15 @@
 const axios = require("axios");
-
+const core = require('@actions/core');
 const header = 'Content-Type: application/json'
 
 let postRequest = async function postMessage(webhookUrl, message) {
     try {
         const data = constructRequestData(message);
         return await axios.post(webhookUrl, data, header)
-            .then(response => response.status);
+            .then(response => {
+                core.info(`Received response: ${response.status} from Teams server`)
+                return response.status
+            });
     } catch (error) {
         throw new Error(`Error sending POST request to Teams \n ${error}`)
     }
