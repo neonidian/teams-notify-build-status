496d61d905bc8c667f01009c5dc4f35549c88351
Keerthi Shankar Subramanian <keerthi.subramanian@mitigram.com>
2022-04-13 19:28:21 +0200

    #19 Enhancement: Add optional title

--- src/payload/payload.js
+++ src/payload/payload.js
@@ -1,7 +1,13 @@
 const CustomizeCard = require("./customize-card/CustomizeCard");
 
-let payLoad = function constructPayload(message, { status, }) {
-    return new CustomizeCard(message, { status, }).constructCard();
+let payLoad = function constructPayload(message, {
+    status,
+    title,
+}) {
+    return new CustomizeCard(message, {
+        status,
+        title,
+    }).constructCard();
 };
 
 module.exports = payLoad;
---
+++ test/http-request-tests/title-enabled.test.js
@@ -0,0 +1,39 @@
+const main = require('../../src/main');
+
+describe('Title enabled', () => {
+    const _teamsIncomingHookUrl = process.env.TEAMS_TEST_HOOK_URL;
+    const responseBody = 1;
+    const SHOULD_DISPLAY_VIEW_COMMIT_BUTTON = "SHOULD_DISPLAY_VIEW_COMMIT_BUTTON";
+    const SHOULD_DISPLAY_VIEW_RUN_BUTTON = "SHOULD_DISPLAY_VIEW_RUN_BUTTON";
+
+    beforeAll(() => {
+        if(!_teamsIncomingHookUrl) {
+            throw new Error('Teams webhook url not found in environment variable "TEAMS_TEST_HOOK_URL"');
+        }
+    });
+
+    beforeEach(() => {
+        delete process.env[SHOULD_DISPLAY_VIEW_COMMIT_BUTTON];
+        delete process.env[SHOULD_DISPLAY_VIEW_RUN_BUTTON];
+    });
+
+    test('Both buttons and title enabled with success status', async () => {
+        process.env = Object.assign(process.env, { [SHOULD_DISPLAY_VIEW_COMMIT_BUTTON]: 'true', [SHOULD_DISPLAY_VIEW_RUN_BUTTON]: 'true' });
+        const messageToSend = 'With title and both buttons enabled; success status';
+        let response = await main(_teamsIncomingHookUrl, messageToSend, {
+            status: 'success',
+            title: 'Docker build workflow'
+        });
+        expect(response).toBe(responseBody);
+    });
+
+    test('Title enabled with no status, view run button enabled', async () => {
+        process.env = Object.assign(process.env, { [SHOULD_DISPLAY_VIEW_RUN_BUTTON]: 'true' });
+        const messageToSend = 'Both buttons to be visible with no status';
+        let response = await main(_teamsIncomingHookUrl, messageToSend, {
+            status: '',
+            title: 'Only with title and view run button'
+        });
+        expect(response).toBe(responseBody);
+    });
+});
